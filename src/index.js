d8922550bb10d59480484121962d5ba8457a16f8
Nabeel on the Lenovo <you@example.com>
2018-11-25 20:29:46 +0000

    addCar insert if not exists

--- src/index.js
+++ src/index.js
@@ -10,17 +10,33 @@ const resolvers = {
     Mutation: {
         addCar: async (parent, args, context, info) => {
             console.log(args.make);
-            const make = await context.db.carMakes({where: {name: args.make}});
-            const model = await context.db.carModels({where: {name: args.model, make:{name:args.make}}});
-            const trim = await context.db.carTrims({where: {name: args.trim, model:{name:args.model, make:{name:args.make}}}})
+            let make = await context.db.carMakes({where: {name: args.make}});
+            let model = await context.db.carModels({where: {name: args.model, make:{name:args.make}}});
+            let trim = await context.db.carTrims({where: {name: args.trim, model:{name:args.model, make:{name:args.make}}}})
 
-            if(make.length == 1 && make[0] != null) {
-                //const makeInsert = 
+            if(make.length > 1 || model.length > 1 || trim.length > 1){
+                throw new Error(`Make/Model/Trim returned more than 1 result ${make}, ${model}, ${trim}`);
+            }
+
+            make = make[0];
+            model = model[0];
+            trim = trim[0];
+
+            if(make == null) {
+                make = await context.db.createCarMake({name: args.make});
+            }
+
+            if(model == null) {
+                model = await context.db.createCarModel({name: args.model, make: {connect: {id: make.id}}});
+            }
+
+            if(trim == null) {
+                trim = await context.db.createCarTrim({name: args.trim, model: {connect: {id: model.id}}})
             }
             console.log(make);
             console.log(model);
             console.log(trim);
-            return make[0];
+            return make;
         }
     }
 }
