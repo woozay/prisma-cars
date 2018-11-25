dcaa02988648e4d6cfbea0e6dab6502d1c92b5ad
Nabeel on the Lenovo <you@example.com>
2018-11-25 18:26:52 +0000

    Added more to insert

--- src/index.js
+++ src/index.js
@@ -11,8 +11,15 @@ const resolvers = {
         addCar: async (parent, args, context, info) => {
             console.log(args.make);
             const make = await context.db.carMakes({where: {name: args.make}});
+            const model = await context.db.carModels({where: {name: args.model, make:{name:args.make}}});
+            const trim = await context.db.carTrims({where: {name: args.trim, model:{name:args.model, make:{name:args.make}}}})
 
+            if(make.length == 1 && make[0] != null) {
+                //const makeInsert = 
+            }
             console.log(make);
+            console.log(model);
+            console.log(trim);
             return make[0];
         }
     }
